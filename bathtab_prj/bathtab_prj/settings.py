3ce74d1edc27dd67841a99e5f780a8f9e2dbd7ed
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-01-01 13:16:17 +0200

    Use Europe/Kyiv"
    -TIME_ZONE = "UTC"
    +TIME_ZONE = "Europe/Kyiv"

--- bathtab_prj/bathtab_prj/settings.py
+++ bathtab_prj/bathtab_prj/settings.py
@@ -104,11 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kyiv"
 
 USE_I18N = True
-
 USE_TZ = True
 
 
